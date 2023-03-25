5de9abef344e1c1773e8f1b57cc4028354bbe62e
Alomba-epongseh <alombangwingche@gmail.com>
2023-03-25 07:44:17 +0100

    my projects

---
+++ beautysite/script.js
@@ -0,0 +1,55 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+
+
+
+//hiding and opening menu bar
+var headerLinks = document.getElementById("headerLinks");
+
+function hideMenu(){
+  headerLinks.style.right = "-250px";
+}
+function showMenu(){
+  headerLinks.style.right = "0";
+}
